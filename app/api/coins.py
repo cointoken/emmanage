f9c05123b0778fa10bb338f038a2e05ad8e5af1e
coder188 <coder188@outlook.com>
2018-08-26 23:36:15 +0800

    '添加币转账成功功能‘

---
+++ app/api/coins.py
@@ -0,0 +1,76 @@
+import requests
+import json
+from datetime import datetime
+import time
+
+class Coins():
+    
+    @staticmethod
+    def get_curr_seconds():
+        return int(time.mktime(datetime.now().timetuple()))
+
+
+    @staticmethod
+    def btc_transactions(address,amount):
+        if address and amount >0:
+            btc_url = 'https://bitaps.com/api/address/transactions/{0}'.format(address)
+            r = requests.get(btc_url)
+            if r.content:
+                js = json.loads(r.content)
+                if js:
+                    amount = amount * 100000000
+                    now_seconds = Coins.get_curr_seconds()
+                    for j in js:
+                        if now_seconds-j[0]<1800 and j[3]=='received' and j[4]=='confirmed' and j[7]==amount:
+                            return j[1]
+
+
+    @staticmethod
+    def eth_get_transaction(address,amount):
+        result = ''
+        if address and amount>0:
+            eth_url = 'http://api.ethplorer.io/getAddressTransactions/{0}'.format(address)
+            params = {'apiKey':'freekey'}
+            r = requests.get(eth_url,params=params)
+            rc = r.content
+            if rc :
+                js = json.loads(rc)
+                try:
+                    if js:
+                        now_seconds = Coins.get_curr_seconds() 
+                        for j in js:
+                            if now_seconds-j['timestamp']<1800 and j['to']==address.lower() and j['value']==amount and j['success']==True:
+                                return j['hash']
+                except:
+                    result = 'error'
+        return result
+
+    
+    @staticmethod
+    def etc_get_transaction(address,amount):
+        result = ''
+        if address and amount>0:
+            etc_url = 'https://api.gastracker.io/v1/addr/{0}/operations'.format(address)
+            r = requests.get(etc_url)
+            js = json.loads(r.content)
+            if js:
+                try:
+                    js = js['items']
+                    now_date = datetime.today().strftime('%Y-%m-%d')
+                    for j in js:
+                       if j['timestamp'].find(now_date)!=-1 and j['to']==address and j['value']['ether']==amount and j['isSend']==False and j['failed']==False:
+                           return j['hash']
+                except:
+                    result = 'error'
+        return result          
+
+
+# if __name__=='__main__':
+#     # #Coins.btc_transactions('18cBEMRxXHqzWWCxZNtU91F5sbUNKhL5PX',12.53265359)
+#     # #print(Coins.etc_get_transaction('0xad505C8e97F0E53C20f6b6D49f74c1ccB8EC998F',0.02))
+#     # #print(int(time.mktime(datetime.now().timetuple())))
+#     # today = datetime.today().strftime('%Y-%m-%d')
+#     # cc = "2018-08-26T03:24:11"
+#     # #@print(today)
+#     # if cc.find(today)!=-1:
+    #     print(today)
